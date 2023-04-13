35bda6fe3f33a6b388c3f32baf076dc5b2b6d72a
Aureli-Rio <servant.aurelien.developer@gmail.com>
2023-04-13 13:46:12 +0200

    RSM - v.02

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/screen_title.css">
+    <title>Riozacki's stealth mission</title>
+</head>
+
+<body class="scanlines">
+    <section id="screen_title">
+        <h4 id="game_title">Riozacki's stealth mission</h4>
+        <div id="button_container_style">
+            <button id="BM1PLAYERS" class="button_style">1 PLAYER</button>
+            <button id="BM2PLAYERS" class="button_style">2 PLAYERS</button>
+            <button id="BMOPTIONS" class="button_style">OPTIONS</button>
+            <button id="BMCREDITS" class="button_style">CREDITS</button>
+        </div>
+    </section>    
+</body>
+
+</html>
